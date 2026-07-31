03bc31920184f17458ffe0135737aef53989b1a9
Claudionor Freitas <netosousa2011@hotmail.com>
2026-07-30 22:01:58 -0300

    Task #2 - criação das rotas e componente de autenticação [em progresso]

---
+++ src/pages/Autenticacao/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './styles.scss';
+import FormularioLogin from "../Components/FormularioLogin";
+
+function Autenticacao() {
+
+  return (
+    <>
+        <div className="container">
+            <FormularioLogin/>
+        </div>
+    </>
+    
+  );
+}
+export default Autenticacao;

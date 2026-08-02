2e91c9aa358ab1dccb6072e806ce3123c1da97ca
Vanderlan Domingos da Silva <vanderlan.domingos@outlook.com>
2026-08-02 20:30:57 -0300

    task #4 - execucao do html e scss apos a aula

--- src/Pages/AutenticacaoAula/index.tsx
+++ src/Pages/AutenticacaoAula/index.tsx
@@ -1,7 +1,32 @@
+import "./styles.scss"
+
 export function AutenticacaoAula () {
 
         return (
 
-            <div> faça um form</div>
+            <div className="container-aula">
+                <form className="login-container">
+
+                <h1 className="login-title">Autenticação</h1>
+                <h2 className="login-subtitle">Bem-vindo ao sistema de autenticação!</h2>
+
+                <div className="input-container">
+                    <input type="e-mail" placeholder="E-mail" required />
+                    <input type="password" placeholder="Senha" required />
+                </div>
+
+                <div className="button-container">
+                    <button className="bt-entrar" type="submit">Entrar</button>
+                    <button className="bt-esqueci-senha" type="button">Esqueci a senha</button>
+                </div>
+
+                <div className="checkbox-container">
+                    <input type="checkbox" id="lembre-me" />
+                    <span className="checkbox-label">Lembrar minha senha</span>
+                </div>
+
+                </form>
+
+            </div>
         )
 }
